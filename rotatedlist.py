c695ceb527b7e1a189d0de55da1f4356db2314fb
Marcel Grossmann <marcelgrossmann80@gmail.com>
2022-07-06 05:20:54 +0100

    solve the rotated list problem with binary search

--- rotatedlist.py
+++ rotatedlist.py
@@ -1,4 +1,4 @@
-def count_rotations_linear(nums):
+def count_rotations_linear(arr):
     '''
     Count rotations.
 
@@ -11,11 +11,33 @@ def count_rotations_linear(nums):
     Return: An integer
     '''
     position = 0
-    while position < len(nums):
-        if position > 0 and nums[position] < nums[position-1]:
+    while position < len(arr):
+        if position > 0 and arr[position] < arr[position-1]:
             return position
         position +=1
     return 0
 
 
-print(count_rotations_linear([4,5,6,1,2,3]))
+def count_rotations_binary(arr):
+    '''Count rotations.
+    
+    Counts the number of rotations made from a sorted list to get a given list using
+    binary search
+    
+    Args: List of numbers.
+    
+    Return: Integer'''
+    lo, hi = 0, len(arr)-1
+    mid = (lo + hi)//2
+    mid_num = arr[mid]
+    while lo < hi:
+        if mid > 0 and arr[mid] < arr[mid-1]:
+            return mid
+        elif arr[mid] > arr[mid-1]:
+            hi = mid -1
+        else:
+            lo = mid +1
+    return 0
+
+
+print(count_rotations_binary([3,4,5,1,2]))
